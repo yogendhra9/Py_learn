65402f2e0098f83c966d025d69c4f6854cc7c352
Yogendhra <yogendhra009@gmail.com>
2025-03-29 01:20:24 +0530

    Added subplots to panda.py for better visualization of the data in the pandas dataframe.

--- panda.py
+++ panda.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 # Load the dataset
 df = pd.read_csv(r"C:\Users\yogen\Downloads\iris.csv")
 
@@ -13,8 +14,20 @@ print(f"Petal Length: {df['PetalLengthCm'].mean():.2f}")
 # Group by species for SepalLengthCm
 print("\nAverage Sepal Length by Species:")
 k = df.groupby("Species")["SepalLengthCm"].mean()
-k.plot(kind="bar", title="Average Sepal Length by Species", color=['#FF9999', '#66B2FF', '#99FF99'])
+l = df.groupby("Species")['SepalWidthCm'].mean()
+plt.subplots_adjust(hspace=1
+                    )  # Adjust space between subplots
+plt.subplot(3,1,1)
+l.plot(kind="bar", title="Average Sepal Width by Species", color=['#FF9999', '#66B2FF', '#99FF99'])
+plt.ylabel("Sepal Width (cm)")
+plt.subplot(3, 1, 2)  #this line should always be there above this 
+plt.plot(k,l, marker='o', linestyle='--', color='b')
+plt.title("Average Sepal Length by Species")
+plt.ylabel("Sepal Width (cm)")
+plt.xlabel("Sepal Length (cm)")
 plt.ylabel("Sepal Length (cm)")
+plt.subplot(3,1,3)  #this line should always be there above this
+k.plot(kind="bar", title="Average Sepal Length by Species",color=['#FF9999', '#66B2FF', '#99FF99'])
 plt.show()
 input("Press Enter to close...")
 print(k)
@@ -34,3 +47,24 @@ virginica = df[df['Species'] == 'Iris-virginica'].mean(numeric_only=True)  # Avo
 print(virginica)
 avg_sepal_length = virginica['SepalLengthCm']
 print(f"Average Sepal Length of Iris-virginica: {avg_sepal_length:.2f}")
+
+
+# xpoints = np.array([1,4,3,5])
+# ypoints = np.array([1, 4, 3, 4])
+# plt.subplot(2,1,1)
+# plt.plot(xpoints, ypoints,marker= 'o',color='g',linestyle='dashed',linewidth=2,markersize=12)
+# plt.title("Simple Plot")
+# plt.xlabel("x-axis")
+# plt.ylabel("y-axis")
+# plt.grid(color='gray', linestyle='--', linewidth=0.5)
+
+# plt.show()
+
+# y1 = np.array([10.4, 20.5, 30.6, 40.7, 50.8])
+# y2 = np.array([12.3, 45.6, 23.4, 56.7, 34.5])
+# plt.subplot(2,1,2)
+# plt.plot(y1,y2,marker= 'o',color='c',linestyle='dotted',linewidth=2,markersize=12)
+# plt.title("Simple Plot")
+# plt.xlabel("x-axis")
+# plt.ylabel("y-axis")
+# plt.show()
