7d54ca1a7cb4d94dfdf1bec6fa63ec1025ad8c7f
Yogendhra <yogendhra009@gmail.com>
2025-03-29 01:24:49 +0530

    Scatter plots also show the distribution of data points in two dimensions.

--- panda.py
+++ panda.py
@@ -48,6 +48,18 @@ print(virginica)
 avg_sepal_length = virginica['SepalLengthCm']
 print(f"Average Sepal Length of Iris-virginica: {avg_sepal_length:.2f}")
 
+#day one, the age and speed of 13 cars: 
+x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+plt.scatter(x, y)
+
+#day two, the age and speed of 15 cars:
+x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
+y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
+plt.scatter(x, y)
+
+plt.show()
+
 
 # xpoints = np.array([1,4,3,5])
 # ypoints = np.array([1, 4, 3, 4])
